cc9b89f7175a96a60d8ef0fe75220695066b2112
David Kim <jk2537@cornell.edu>
2020-02-01 19:33:33 -0500

    Version 2.0
    - Completely reformatted and restructured code.

--- FallDetectionEx/FallDetectionEx.ino
+++ FallDetectionEx/FallDetectionEx.ino
@@ -1,23 +1,22 @@
 #include <Wire.h>
 const int MPU_addr = 0x68;  // I2C address of the MPU-6050
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-float   ax=0, ay=0, az=0, gx=0, gy=0, gz=0;
+float ax = 0, ay = 0, az = 0, gx = 0, gy = 0, gz = 0;
 
-//int data[STORE_SIZE][5]; //array for saving past data
-//byte currentIndex=0; //stores current data array index (0-255)
-boolean fall     = false; //stores if a fall has occurred
-boolean trigger1 = false; //stores if first trigger (lower threshold) has occurred
-boolean trigger2 = false; //stores if second trigger (upper threshold) has occurred
-boolean trigger3 = false; //stores if third trigger (orientation change) has occurred
+boolean fall = false;
+boolean trigger0 = true;
+boolean trigger1 = false;
+boolean trigger2 = false;
+boolean trigger3 = false;
 
-byte trigger1count = 0; //stores the counts past since trigger 1 was set true
-byte trigger2count = 0; //stores the counts past since trigger 2 was set true
-byte trigger3count = 0; //stores the counts past since trigger 3 was set true
-int angleChange    = 0;
+byte trigger1Count = 0;
+byte trigger2Count = 0;
+byte trigger3Count = 0;
+int angleChange = 0;
 
-long acc_x_cal = 0;
-long acc_y_cal = 0;
-long acc_z_cal = 0;
+long avgAcX = 0;
+long avgAcY = 0;
+long avgAcZ = 0;
 
 void setup() {
   Wire.begin();
@@ -25,117 +24,108 @@ void setup() {
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
-  Serial.begin(9600);
 
+//  calibrate(3); // takes input # seconds
+//  Serial.print("Average of AcX: " + (String) avgAcX + "\n");
+//  Serial.print("Average of AcY: " + (String) avgAcY + "\n");
+//  Serial.println("Average of AcZ: " + (String) avgAcZ + "\n");
+  
   pinMode(11, OUTPUT);
   digitalWrite(11, LOW);
-
-// calibrate(2); // takes input # seconds
-// Serial.print("Average of AcX: " + (String) acc_x_cal + "\n");
-// Serial.print("Average of AcY: " + (String) acc_y_cal + "\n");
-// Serial.println("Average of AcZ: " + (String) acc_z_cal + "\n");
+  
+  Serial.begin(9600);
 }
 
 void loop(){
-  mpu_read();
-  // 2050, 77, 1947 are values for calibration of accelerometer
-  // values may be different for you. 16384.00 divide.
-  ax = (AcX - acc_x_cal) / 16384.00;
-  ay = (AcY - acc_y_cal) / 16384.00;
-  az = (AcZ - acc_z_cal) / 16384.00;
-//  Serial.println("X: " + (String) ax);
-//  Serial.println("Y: " + (String) ay);
-//  Serial.println("Z: " + (String) az + "\n");
+  mpuRead();
+  // 2050, 77, 1947 are original values for calibrating the accelerometer manually.
+  // Values may be different based on config. Divide by 16384.0
+  ax = (AcX - avgAcX) / 16384.;
+  ay = (AcY - avgAcY) / 16384.;
+  az = (AcZ - avgAcZ) / 16384.;
+//  Serial.println("AcX: " + (String) ax);
+//  Serial.println("AcY: " + (String) ay);
+//  Serial.println("AcZ: " + (String) az + "\n");
 
-  //270, 351, 136 for gyroscope
-  gx = (GyX + 270) / 131.07;
-  gy = (GyY - 351) / 131.07;
-  gz = (GyZ + 136) / 131.07;
+  // 270, - 351, 136 for gyroscope
+  gx = (GyX) / 131.07;
+  gy = (GyY) / 131.07;
+  gz = (GyZ) / 131.07;
+//  Serial.println("GyX: " + (String) gx);
+//  Serial.println("GyY: " + (String) gy);
+//  Serial.println("GyZ: " + (String) gz + "\n");
+  
 
-  // calculating Amplitute vactor for 3 axis
-  float Raw_AM = pow(ax * ax + ay * ay + az * az, 0.5);
-  float AM = Raw_AM * 10; // as values are within 0 to 1, I multiplied 
-                          // it by for using if else conditions 
+  // amplitude vector for the 3 axes
+  float rawAM = pow(ax * ax + ay * ay + az * az, 0.5);
+  float AM = rawAM * 10; // scaled AM
   
   Serial.println(AM);
-  //Serial.println(PM);
-  //delay(500);
+//  float rawGyro = pow(gx * gx + gy * gy + gz * gz, 0.5);
+//  Serial.println(rawGyro);
+//  delay(500);
 
-  if (trigger3 == true){
-    trigger3count++;
-    //Serial.println(trigger3count);
-    if (trigger3count >= 10) { 
-       angleChange = pow(gx * gx + gy * gy + gz * gz, 0.5);
-       delay(10);
-       //Serial.println("Angle change: " + angleChange); 
-       if ((angleChange >= 0) && (angleChange <= 10)) { //if orientation changes remains between 0-10 degrees
-           fall          = true; 
-           trigger3      = false; 
-           trigger3count = 0;
-       } else { // user regained normal orientation
-          trigger3      = false; 
-          trigger3count = 0;
-          Serial.println("TRIGGER 3 DEACTIVATED");
-       }
-     }
-    fall = true;
-  }
-  
-  if (fall == true) { //in event of a fall detection
-    Serial.println("FALL DETECTED");
-    digitalWrite(11, HIGH);
-    delay(1000);
-    digitalWrite(11, LOW);
- 
-    fall = false;
-    // exit(1);
-  }
-
-  if (trigger2count > 5) { // allow 0.5s for orientation change
-    trigger2      = false; 
-    trigger2count = 0;
-    Serial.println("TRIGGER 2 DEACTIVATED");
-  }
- 
-  if (trigger1count > 5) { // allow 0.5s for AM to break upper threshold
-    trigger1      = false; 
-    trigger1count = 0;
-    Serial.println("TRIGGER 1 DEACTIVATED");
-  }
-   
-  if (trigger2 == true) {
-    trigger2count++;
-    // angleChange=acos(((double)x*(double)bx+(double)y*(double)by+(double)z*(double)bz)/(double)AM/(double)BM);
-    angleChange = pow(gx * gx + gy * gy + gz * gz, 0.5); 
-    
-    if (angleChange >= 30 && angleChange <= 400) { //if orientation changes by between 80-100 degrees
-      trigger3      = true; 
-      trigger2      = false; 
-      trigger2count = 0;
-      Serial.println("TRIGGER 3 ACTIVATED");
+  // First step: free fall
+  if (trigger0) {
+    if (AM <= 3) { // if AM breaks lower threshold (0.4g)
+      trigger0 = false;
+      trigger1 = true;
+      Serial.println("TRIGGER 1 ACTIVATED");
     }
-  }
-
-  if (trigger1 == true) {
-    trigger1count++;
-    if (AM >= 12) { //if AM breaks upper threshold (3g)
+  } else if (trigger1) {
+    trigger1Count++;
+    if (AM >= 12) { // if AM breaks upper threshold (3g)
+      trigger1 = false;
+      trigger1Count = 0;
       trigger2 = true;
       Serial.println("TRIGGER 2 ACTIVATED");
-      trigger1      = false; 
-      trigger1count = 0;
+    } else if (trigger1Count >= 5) {
+      trigger1 = false; 
+      trigger1Count = 0;
+      Serial.println("TRIGGER 1 DEACTIVATED");
     }
+  } else if (trigger2) {
+    trigger2Count++;
+    angleChange = pow(gx * gx + gy * gy + gz * gz, 0.5); 
+    if (angleChange >= 30 && angleChange <= 400) { // if orientation changes by between 80-100 degrees
+      trigger2 = false; 
+      trigger2Count = 0;
+      trigger3 = true; 
+      Serial.println("TRIGGER 3 ACTIVATED");
+    } else if (trigger2Count > 5) { // allow 0.5s for orientation change
+      trigger2 = false; 
+      trigger2Count = 0;
+      Serial.println("TRIGGER 2 DEACTIVATED");
+      }
+  } else if (trigger3) {
+    trigger3Count++;
+    if (trigger3Count >= 50) { // 5 seconds
+      angleChange = pow(gx * gx + gy * gy + gz * gz, 0.5);
+      delay(10);
+      // if orientation changes remains between 0-20 degrees
+      if (angleChange >= 0 && angleChange <= 20) {
+        trigger3 = false; 
+        trigger3Count = 0;
+        fall = true; 
+      } else { // user regained normal orientation
+        trigger3 = false; 
+        trigger3Count = 0;
+        Serial.println("TRIGGER 3 DEACTIVATED");
+      }
+    }
+  } else if (fall) {
+    Serial.println("FALL DETECTED");
+    digitalWrite(11, HIGH);
+    delay(50);
+    digitalWrite(11, LOW);
+    fall = false;
   }
-   
-  if (AM <= 2 && trigger2 == false) { //if AM breaks lower threshold (0.4g)
-    trigger1 = true;
-    Serial.println("TRIGGER 1 ACTIVATED");
-  }
-
+  
   //It appears that delay is needed in order not to clog the port
   delay(100);
 }
 
-void mpu_read() {
+void mpuRead() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -149,15 +139,17 @@ void mpu_read() {
   GyZ = (Wire.read() << 8) | Wire.read();  // 0x47 (GYRO_ZOUT_H)  & 0x48 (GYRO_ZOUT_L)
 }
 
+// Not fully functional as of right now. 
+// Better left uncalibrated.
 void calibrate(int n) {
   for (int i = 0; i < (n * 333); i++) {
-    mpu_read();
-    acc_x_cal += AcX;
-    acc_y_cal += AcY;
-    acc_z_cal += AcZ;
+    mpuRead();
+    avgAcX += AcX;
+    avgAcY += AcY;
+    avgAcZ += AcZ;
     delay(3);
   }
-  acc_x_cal /= (n * 333);
-  acc_y_cal /= (n * 333);
-  acc_z_cal /= (n * 333);
+  avgAcX /= (n * 333);
+  avgAcY /= (n * 333);
+  avgAcZ /= (n * 333);
 }
